0ae7171edd04ca549c986db7cfe2ed6c1329f40e
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2021-02-23 16:15:59 -0300

    :white_check_mark: Adding test class Frames e Janelas

--- TestFramesEJanejas.java
+++ TestFramesEJanejas.java
@@ -25,4 +25,39 @@ public class TestFramesEJanejas {
 		driver.quit();
 		
 	}
+	
+	@Test
+	public void testJanelasComTitulo() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("buttonPopUpEasy")).click();
+		driver.switchTo().window("Popup");
+		driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+		driver.close();
+		driver.switchTo().window("");
+		driver.findElement(By.tagName("textarea")).sendKeys("e agora?");
+		
+		driver.quit();	
+	}
+	
+	@Test
+	public void testJanelasSemTitulo() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("buttonPopUpHard")).click();
+		System.out.println(driver.getWindowHandle());
+		System.out.println(driver.getWindowHandles());
+		driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
+		driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+		driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
+		driver.findElement(By.tagName("textarea")).sendKeys("E agora?");
+		
+		driver.quit();
+	}
+	
+	
 }
