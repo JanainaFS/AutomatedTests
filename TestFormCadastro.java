4d4a7ef67a808f494a27532c5ec7836ea1c4e1b4
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2021-02-25 13:57:39 -0300

    :recycle: Refactor code

--- TestFormCadastro.java
+++ TestFormCadastro.java
@@ -2,11 +2,12 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FormCadastro {
+public class TestFormCadastro {
 	private WebDriver driver;
 	private CampoTreinamentoPage page;
 	
@@ -42,6 +43,68 @@ public class FormCadastro {
 		Assert.assertEquals("superior", driver.findElement(By.id("descEscolaridade")).findElement(By.tagName("span")).getText());
 		Assert.assertEquals("Natacao", driver.findElement(By.id("descEsportes")).findElement(By.tagName("span")).getText());
 		Assert.assertEquals("Nenhuma sugestão.", driver.findElement(By.id("descSugestoes")).findElement(By.tagName("span")).getText());
+	}
+	
+	@Test
+	public void validacaoNome() {
+		page.cadastrar();
+		
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Nome eh obrigatorio", alert.getText());
+		alert.accept();
+	}
+	
+	@Test
+	public void validacaoSobrenome() {
+		page.setNome("Janaina");
+		
+		page.cadastrar();
+		
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Sobrenome eh obrigatorio", alert.getText());
+		alert.accept();
+	}
+	
+	@Test
+	public void validacaoSexo() {
+		page.setNome("Janaina");
+		page.setSobrenome("Feitosa"); 
+		
+		page.cadastrar();
+
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Sexo eh obrigatorio", alert.getText());
+		alert.accept();
+	}
+	
+	@Test
+	public void validacaoComida() {
+		page.setNome("Janaina");
+		page.setSobrenome("Feitosa"); 
+		page.setSexoFeminino();
+		page.setComidaFrango();
+		page.setComidaVegetariana();
+		
+		page.cadastrar();
+		
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Tem certeza que voce eh vegetariano?", alert.getText());
+		alert.accept();
+	}
+	
+	@Test
+	public void validacaoEsporte(){
+		page.setNome("Janaina");
+		page.setSobrenome("Feitosa"); 
+		page.setSexoFeminino();
+		page.setComidaFrango();
+		page.setEsporte("Natacao", "O que eh esporte?");
+		
+		page.cadastrar();
+		
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Voce faz esporte ou nao?", alert.getText());
+		alert.accept();
 	}
 	
 }
