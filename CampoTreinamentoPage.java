de7e879e8db944380265275d2d5f0345c94c6903
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2021-03-02 14:44:41 -0300

    :white_check_mark: Add test

--- CampoTreinamentoPage.java
+++ CampoTreinamentoPage.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CampoTreinamentoPage {
@@ -50,5 +51,33 @@ public class CampoTreinamentoPage {
 	
 	public void cadastrar() {
 		dsl.clicarBotao("elementosForm:cadastrar");
+	}
+	
+	public String obterResultadoCadastro(){
+		return dsl.obterTexto(By.xpath("//*[@id='resultado']/span"));
+	}
+	
+	public String obterNomeCadastro(){
+		return dsl.obterTexto(By.xpath("//*[@id='descNome']/span"));
+	}
+	
+	public String obterSobrenomeCadastro(){
+		return dsl.obterTexto(By.xpath("//*[@id='descSobrenome']/span"));
+	}
+	
+	public String obterSexoCadastro(){
+		return dsl.obterTexto(By.xpath("//*[@id='descSexo']/span"));
+	}
+	
+	public String obterComidaCadastro(){
+		return dsl.obterTexto(By.xpath("//*[@id='descComida']/span"));
+	}
+	
+	public String obterEscolaridadeCadastro(){
+		return dsl.obterTexto(By.xpath("//*[@id='descEscolaridade']/span"));
+	}
+	
+	public String obterEsportesCadastro(){
+		return dsl.obterTexto(By.xpath("//*[@id='descEsportes']/span"));
 	}
 }
--- TestFormCadastro.java
+++ TestFormCadastro.java
@@ -2,7 +2,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -30,18 +29,25 @@ public class TestFormCadastro {
 		page.setComidaFrango();
 		page.setEscolaridade("Superior");
 		page.setEsporte("Natacao");
-		page.setSugestao("Nenhuma sugestão.");
 		page.cadastrar();
 		
 		//Refatorar depois
-		Assert.assertEquals("Cadastrado!", driver.findElement(By.id("resultado")).findElement(By.tagName("span")).getText());
-		Assert.assertEquals("Janaina", driver.findElement(By.id("descNome")).findElement(By.tagName("span")).getText());
-		Assert.assertEquals("Feitosa", driver.findElement(By.id("descSobrenome")).findElement(By.tagName("span")).getText());
-		Assert.assertEquals("Feminino", driver.findElement(By.id("descSexo")).findElement(By.tagName("span")).getText());
-		Assert.assertEquals("Frango", driver.findElement(By.id("descComida")).findElement(By.tagName("span")).getText());
-		Assert.assertEquals("superior", driver.findElement(By.id("descEscolaridade")).findElement(By.tagName("span")).getText());
-		Assert.assertEquals("Natacao", driver.findElement(By.id("descEsportes")).findElement(By.tagName("span")).getText());
-		Assert.assertEquals("Nenhuma sugestão.", driver.findElement(By.id("descSugestoes")).findElement(By.tagName("span")).getText());
+//		Assert.assertEquals("Cadastrado!", driver.findElement(By.id("resultado")).findElement(By.tagName("span")).getText());
+//		Assert.assertEquals("Janaina", driver.findElement(By.id("descNome")).findElement(By.tagName("span")).getText());
+//		Assert.assertEquals("Feitosa", driver.findElement(By.id("descSobrenome")).findElement(By.tagName("span")).getText());
+//		Assert.assertEquals("Feminino", driver.findElement(By.id("descSexo")).findElement(By.tagName("span")).getText());
+//		Assert.assertEquals("Frango", driver.findElement(By.id("descComida")).findElement(By.tagName("span")).getText());
+//		Assert.assertEquals("superior", driver.findElement(By.id("descEscolaridade")).findElement(By.tagName("span")).getText());
+//		Assert.assertEquals("Natacao", driver.findElement(By.id("descEsportes")).findElement(By.tagName("span")).getText());
+//		Assert.assertEquals("Nenhuma sugestão.", driver.findElement(By.id("descSugestoes")).findElement(By.tagName("span")).getText());
+		
+		Assert.assertEquals("Cadastrado!", page.obterResultadoCadastro());
+		Assert.assertEquals("Janaina", page.obterNomeCadastro());
+		Assert.assertEquals("Feitosa", page.obterSobrenomeCadastro());
+		Assert.assertEquals("Feminino", page.obterSexoCadastro());
+		Assert.assertEquals("Frango", page.obterComidaCadastro());
+		Assert.assertEquals("superior", page.obterEscolaridadeCadastro());
+		Assert.assertEquals("Natacao", page.obterEsportesCadastro());
 	}
 		
 }
