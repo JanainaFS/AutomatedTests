90a971f6f88819c421111bdf311eeefb6bb90579
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2021-03-05 16:18:08 -0300

    :white_check_mark: Adding to the property class choice of browsers

--- core/DriverFactory.java
+++ core/DriverFactory.java
@@ -2,6 +2,7 @@ package br.janaina.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 	private static WebDriver driver;
@@ -10,7 +11,10 @@ public class DriverFactory {
 	
 	public static WebDriver getDriver() {
 		if(driver == null) {
-			driver = new ChromeDriver();
+			switch (Properties.browser) {
+				case CHROME: driver = new ChromeDriver(); break;
+				case FIREFOX: driver = new FirefoxDriver(); break;
+			}
 		}
 		return driver;
 	}
--- core/Properties.java
+++ core/Properties.java
@@ -3,4 +3,11 @@ package br.janaina.core;
 public class Properties {
 	
 	public static boolean FECHAR_BROWSER = false;
+	
+	public static Browsers browser = Browsers.CHROME;
+	
+	public enum Browsers {
+		CHROME,
+		FIREFOX
+	}
 }
